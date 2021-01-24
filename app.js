54bbb7e57a37c5450bd0da5f1fa9abab27f586aa
Sho Maruyama <mrsho34@gmail.com>
2021-01-24 13:47:24 +0900

    add: http-only in set-cookie

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app_first = express();
 app_first.use(express.static('first-party', {
     setHeaders: (res, path, stat) => {
-        res.cookie('first-paty', 'cookie');
+        res.cookie('first-paty', 'cookie', {
+            httpOnly: true,
+        });
     },
 }));
 app_first.listen(8080, function(){
@@ -15,6 +17,7 @@ app_third.use(express.static('third-party', {
     setHeaders: (res, path, stat) => {
         res.cookie('third-paty', 'cookie', {
             sameSite: 'none',
+            httpOnly: true,
             secure: true,
         });
     },
